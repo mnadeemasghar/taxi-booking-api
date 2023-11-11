494cce51a7db876e151e102293ef099c57def271
M Nadeem Asghar <m.nadeem.asghar@hotmail.com>
2023-11-11 14:18:34 +0500

    api route name removed

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/login',[AuthController::class,'login'])->name('login');
-Route::post('/register',[AuthController::class,'register'])->name('register');
+Route::post('/login',[AuthController::class,'login']);
+Route::post('/register',[AuthController::class,'register']);
 
-Route::post("/",[Controller::class,'home'])->name('home');
+Route::post("/",[Controller::class,'home']);
